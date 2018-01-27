c088215bd5f704c57f99ac2a2f2931e7bc86d89e
Anthony Luu <a6luu@uwaterloo.ca>
2018-01-27 11:03:05 -0500

    kind of working

---
+++ tileset/grass-tile-2-small.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass-tile-2-small" tilewidth="32" tileheight="32" tilecount="72" columns="12">
+ <image source="tileset/grass-tiles-2-small.png" width="384" height="192"/>
+ <tile id="17" type="lighthouse"/>
+ <tile id="18" type="main"/>
+</tileset>
