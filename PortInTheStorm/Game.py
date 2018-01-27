d20d6addb523f51a8a6b3499b6faad2dfc7bc2d4
Anthony Luu <a6luu@uwaterloo.ca>
2018-01-27 14:31:29 -0500

    added test sprites. added ability to load the different types of sprites based on the tmx custom properties

--- PortInTheStorm/Game.py
+++ PortInTheStorm/Game.py
@@ -2,6 +2,7 @@
 import pygame
 import pytmx
 from pytmx import load_pygame
+from TowerEntity import CreateDefaultTowerEntity
 
 class RegionData:
     def __init__(self):
@@ -12,14 +13,39 @@ class Game:
         print("Loading level " + initial_level.level_properties.level_name)
         initial_level.load()
         self.current_level = initial_level
-        self.region_data = RegionData()        
+        self.region_data = RegionData()
+        self.all_sprites = pygame.sprite.Group()
+        self.initialize_lighthouses()
 
-    # Returns false 
+    # This method iterates over the game map only once to create sprites based on the property 'sprite_type'
+    # sprite_types can be:
+    # - "lighthouse"
+    # - "main"
+    # - "ship"
+    #
+    # These sprite_types should be custom types added in the Tiled app (or the tmx editing app) for the tileset
+    def initialize_lighthouses(self):
+        game_map = self.current_level.loaded_sprite_map
+        for layer in game_map.visible_layers:
+            for x in range(0, 20):
+                for y in range(0, 20):
+                    properties = game_map.get_tile_properties(x, y, 0)
+                    if properties:
+                        if properties['sprite_type'] == "lighthouse":
+                            towerEntity = CreateDefaultTowerEntity(x,y, self.region_data, True)
+                            self.all_sprites.add(towerEntity.sprite)
+                            continue
+                        if properties['sprite_type'] == "main":
+                            towerEntity = CreateDefaultTowerEntity(x,y, self.region_data, False)
+                            self.all_sprites.add(towerEntity.sprite)
+                            continue
+
+    # Returns false
     def HandleInputEvents(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
-                    
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
                     return False
@@ -36,6 +62,10 @@ class Game:
                     for y in range(0, 20):
                         pygame_surface = game_map.get_tile_image(x, y, 0)
                         screen.blit(pygame_surface, (32*x, 32*y))
+
+        # render all the sprites
+        self.all_sprites.draw(screen)
+
         pygame.display.flip()
 
     def GameTick(self):
--- PortInTheStorm/TowerEntity.py
+++ PortInTheStorm/TowerEntity.py
@@ -2,16 +2,31 @@
 import pygame
 import pytmx
 from pytmx import load_pygame
+from Sprite import Sprite
+
+DEFAULT_LIGHT_RECIEVER_ANGLES = [False, True, True, True, True, True, True, True]
+DEFAULT_LIGHT_TRANSMITTER_ANGLES = [True, False, False, False, False, False, False, False]
+
+def CreateDefaultTowerEntity(x, y, region_data, is_reciever):
+    # creates a default tower and appends it to region_data
+    towerType = TowerType(DEFAULT_LIGHT_RECIEVER_ANGLES, DEFAULT_LIGHT_TRANSMITTER_ANGLES, 0, is_reciever)
+    towerEntity = TowerEntity(x, y, None, towerType)
+    region_data.region_entities.append(towerEntity)
+    return towerEntity
 
 class TowerType:
     def __init__(self, enabled_light_reciever_angles, enabled_light_transmittor_angles, initial_rotation, is_reciever):
         self.enabled_light_reciever_angles = enabled_light_reciever_angles
         self.enabled_light_transmittor_angles = enabled_light_transmittor_angles
         self.initial_rotation = initial_rotation
-        
+        self.is_reciever = is_reciever
+
 class TowerEntity:
     def __init__(self, x, y, state, tower_type):
         self.x = x
         self.y = y
         self.tower_type = tower_type
-
+        # pos is placeholder
+        self.sprite = Sprite(pos=0, frames=[[pygame.image.load('sprites/whitesprite.png')]])
+        self.sprite.rect.x = x*32
+        self.sprite.rect.y = y*32
