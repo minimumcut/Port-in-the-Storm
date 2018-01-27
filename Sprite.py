50cea789cb4782f7c211485e6fdc2b1e55d25971
minimumcut <tinnymint@gmail.com>
2018-01-27 11:23:45 -0500

    Sprite with support for animation

---
+++ Sprite.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, pos, frames=None):
+        super(Sprite, self).__init__()
+        self.frames = frames
+        self.animation = self.stand_animation()
+        self.image = frames[0][0]
+        self.rect = self.image.get_rect()
+        self.pos = pos
+
+    def stand_animation(self):
+        while True:
+            for frame in self.frames[0]:
+                self.image = frame
+                yield None
+                yield None
+
+    def update(self, *args):
+        self.animation.next()
